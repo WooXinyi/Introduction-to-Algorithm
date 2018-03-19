2fb93a023dfbce4533f880693e523c8e1a894fd0
WooXinyi <wuxinyi12358@gmail.com>
2018-03-19 17:56:23 +0800

    some modification

---
+++ Chapter 4/Find_maximum_subarray.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 19 17:17:52 2018
+
+@author: wuxin
+"""
+
+#Method one
+def Find_max_crossing_subarray(nums, low, middle, high):
+    left_sum = float('-inf')
+    sum = 0
+    for i in range(middle, low-1, -1):
+        sum += nums[i]
+        if sum > left_sum:
+            left_sum = sum
+            max_left = i
+    right_sum = float('-inf')
+    sum = 0
+    for j in range(middle+1, high+1):
+        sum += nums[j]
+        if sum > right_sum:
+            right_sum = sum
+            max_right = j
+    return max_left, max_right, left_sum+right_sum
+
+
+def Find_maximum_subarray(nums, low, high):
+    if low == high:
+        return low, high, nums[low]
+    else:
+        mid = int((low+high)/2)
+        left_low, left_high, left_sum = \
+        Find_maximum_subarray(nums, low, mid)
+        right_low, right_high, right_sum = \
+        Find_maximum_subarray(nums, mid+1, high)
+        cross_low, cross_high, cross_sum = \
+        Find_max_crossing_subarray(nums, low, mid, high)
+    if left_sum > right_sum and left_sum > cross_sum:
+        return left_low, left_high, left_sum
+    if right_sum > left_sum and right_sum > cross_sum:
+        return right_low, right_high, right_sum
+    if cross_sum > left_sum and cross_sum > right_sum:
+        return cross_low, cross_high, cross_sum
+    
+nums = [13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7]
+low, high, s = Find_maximum_subarray(nums, 0, len(nums)-1)
+print(low, high, s)
